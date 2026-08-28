18d1190b557fd623697ea6060c3f066d3f95f422
Vu <lenguyenanhvu227@gmail.com>
2026-08-28 20:34:12 +0700

    update

--- Ex4/Program4.cs
+++ Ex4/Program4.cs
@@ -11,21 +11,49 @@
         bool isBirthdayValid = DateTime.TryParseExact(BirthdayInput, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime birthday);
         if (isBirthdayValid == true)
         {
-            int age = DateTime.Now.Year - birthday.Year;
-            if (birthday.Month < DateTime.Now.Month || birthday.Month == DateTime.Now.Month && birthday.Day <= DateTime.Now.Day) 
+            int age = DateTime.Today.Year - birthday.Year;
+            bool isBirthdayPassed;
+            if (birthday.Month < DateTime.Today.Month || birthday.Month == DateTime.Today.Month && birthday.Day <= DateTime.Today.Day)
             {
                 Console.WriteLine($"Tuổi hiện tại: {age} tuổi");
+                isBirthdayPassed = true;
             }
             else
             {
-                Console.WriteLine($"Tuổi hiện tại: {age - 1} tuổi");
+                age--;
+                Console.WriteLine($"Tuổi hiện tại: {age} tuổi");
+                isBirthdayPassed = false;
+            }
+            TimeSpan thoiGianThowr = DateTime.Today - birthday;
+            Console.WriteLine("Bạn đã sống tộng cộng:" + thoiGianThowr.Days + " ngày");
+            DateTime nextBirthday = DateTime.Today;
+            int dayUntilNextBirthday;
+            if (isBirthdayPassed == false)
+            {
+                dayUntilNextBirthday = (birthday - DateTime.Today).Days;
+            }
+            else
+            {
+                nextBirthday = new DateTime(DateTime.Today.Year, birthday.Month, birthday.Day);
+                dayUntilNextBirthday = (nextBirthday - DateTime.Today).Days;
+            }
+            if (birthday.Month == 2 && birthday.Day == 29 && DateTime.IsLeapYear(birthday.Year) == true)
+            {
+                while (DateTime.IsLeapYear(nextBirthday.Year)==false)
+                {
+                    nextBirthday = nextBirthday.AddYears(1);
+                }
+                dayUntilNextBirthday = (nextBirthday - DateTime.Today).Days;
+                Console.WriteLine($"Sinh nhật tiếp theo còn: {dayUntilNextBirthday} ngày");
+            }
+            else
+            {
+                Console.WriteLine($"Sinh nhật tiếp theo còn: {dayUntilNextBirthday} ngày");
             }
         }
         else
         {
             Console.WriteLine("Ngày sinh không hợp lệ. Vui lòng nhập theo định dạng dd/mm/yyyy.");
         }
-        Console.WriteLine("Bạn đã sống tộng cộng:" + (DateTime.Now - birthday).Days + " ngày");
-
     }
 }
---
+++ Ex5/Program4dot5.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine(" ---Input---");
+        Console.Write("Nhập ngày sinh (dd/mm/yyyy): ");
+        string birthdayInput = Console.ReadLine();
+        Console.WriteLine(" ---Output---");
+        bool isBirthdayValid = DateTime.TryParseExact(birthdayInput, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime birthday);
+        if (isBirthdayValid == true)
+        {
+            bool isBirthdayPassed = birthday.Month < DateTime.Today.Month || birthday.Month == DateTime.Today.Month && birthday.Day <= DateTime.Today.Day;
+            int age;
+            if (isBirthdayPassed == true) { age = DateTime.Today.Year - birthday.Year; }
+            else { age = DateTime.Today.Year - birthday.Year - 1; }
+            Console.WriteLine($"Tuổi hiện tại: {age} tuổi");
+            int daysLived = (DateTime.Today - birthday).Days;
+            Console.WriteLine($"Bạn đã sống tổng cộng: {daysLived} ngày");
+            DateTime nextBirthday = new DateTime(DateTime.Today.Year, birthday.Month, birthday.Day);
+            if (birthday.Day == 29 && birthday.Month == 2)
+            {
+                bool isNextBirthdayYearLeap = DateTime.IsLeapYear(nextBirthday.Year);
+                while (isNextBirthdayYearLeap == false)
+                {
+                    nextBirthday.AddYears(1);
+                }
+            }
+            int daysUntilNextBirthday;
+            if (isBirthdayPassed == false)
+            {
+                daysUntilNextBirthday = (nextBirthday - DateTime.Today).Days;
+            }
+            else
+            {
+                nextBirthday = nextBirthday.AddYears(1);
+                daysUntilNextBirthday = (nextBirthday - DateTime.Today).Days;
+            }
+            Console.WriteLine($"Sinh nhật tiếp theo còn {daysUntilNextBirthday:N0} ngày");
+        }
+        else
+        {
+            Console.WriteLine("Ngày sinh không hợp lệ. Vui lòng nhập theo định dạng dd/mm/yyyy.");
+        }
+    }
+    
+}
