3778830f23af2818315d92bf6426b7bc4119b4cf
Vu <lenguyenanhvu227@gmail.com>
2026-09-08 15:30:49 +0700

    well requirements matches as Input always valid Exercise #4

---
+++ 4.2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+2. Write a C# Sharp program to display certain values of the function x = y2 
++ 2y + 1 (using integer numbers for y, ranging from -5 to +5).
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int y = -5;
+        while (y>=-5 && y<=5)
+        {
+            Console.WriteLine($"x = {Math.Pow(y,2)+2*y+1}");
+            y++;
+        }
+    }
+}
---
+++ 4.4/Program.cs
@@ -0,0 +1,14 @@
+﻿/* 
+4. Write a C# Sharp program that takes the radius of a sphere as input and 
+    Volume. V = 4/3*π*r3 
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("---Input---");
+        Console.Write("Radius (m): "); double radius = double.Parse(Console.ReadLine());
+        Console.WriteLine("Sphere surface: "+(Math.Pow(radius,2)*Math.PI*4) );
+        Console.WriteLine("Sphere volume: "+(4d/3d)*Math.PI*Math.Pow(radius,3) );
+    }
+}
